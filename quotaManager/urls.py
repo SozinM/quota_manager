cf54f75eb84c72da12db29e3c573be5138e945b8
Mikhail Sozin <msozin@cloudlinux.com>
2021-07-29 02:38:38 -0400

    Changed some info in swagger docs

--- quotaManager/urls.py
+++ quotaManager/urls.py
@@ -8,12 +8,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Quota Manager",
       default_version='v1',
-      description="Test description",
+      description="Quota Manager",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      contact=openapi.Contact(email="mikawamp@gmail.com"),
+      license=openapi.License(name="MIT License"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
